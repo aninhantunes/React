294c79b7a8ccc222aa2bce164f3787c835944b03
Ana Antunes <aninhaantunes2000@hotmail.com>
2020-06-10 08:39:01 -0300

    página de login

--- src/routers/public/register/index.jsx
+++ src/routers/public/register/index.jsx
@@ -1,14 +1,18 @@
 import React from "react"
-import {Grid,TextField, Paper} from "@material-ui/core"
+import {Grid,TextField, Paper, shadows, Button} from "@material-ui/core"
 import {makeStyles} from '@material-ui/core/styles'
 
 const Register = ({nome}) => {
     const classes = useStyles ();
     return (
-        <Paper>
+        <Paper boxShadow={2} className = {classes.paper}>
             <Grid container spacing = {2} direction = 'column' className = {classes.gridContainer}>
-                <Grid item  >
+                <h1 className = {classes.title}>Login</h1>
+                <Grid item>
                     <TextField id="outlined-basic" label="Nome Completo" variant="outlined" className = {classes.textfield}/>
+                </Grid>
+                <Grid item>
+                    <TextField id="outlined-basic" label="Data de Nascimento" variant="outlined" className = {classes.textfield}/>
                 </Grid>
                 <Grid item>
                     <TextField id="outlined-basic" label="Email" variant="outlined" className = {classes.textfield}/>
@@ -19,9 +23,9 @@ const Register = ({nome}) => {
                 <Grid item>
                     <TextField id="outlined-basic" label="Confirmação de Senha" variant="outlined" type="password" className = {classes.textfield}/>
                 </Grid>
-                <Grid item>
-                    <TextField id="outlined-basic" label="Data de Nascimento" variant="outlined" className = {classes.textfield}/>
-                </Grid>
+                <Button variant="outlined">
+                    Entrar
+                </Button>
             </Grid>
         </Paper>
         
@@ -31,6 +35,11 @@ const Register = ({nome}) => {
 const useStyles = makeStyles((tema) => ({
     gridContainer: {
         width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 'auto',
+        padding: '100px',
+        backgroundColor: '#bfd7ea',
     
     },
 
@@ -39,11 +48,18 @@ const useStyles = makeStyles((tema) => ({
     },
 
     paper: {
-        hight: '600px',
+        hight: '300px',
         width: '500px',
-        padding: tema.spacing(2),
+        justifyContent: 'center',
         margin: 'auto',
+
+    },
+
+    title:{
+        color: '#0b3954'
+
     }
+
 }));
 
 export default Register;
