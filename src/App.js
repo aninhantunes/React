382fead2e33a748e9736d69199dc53b30102fe97
Ana Antunes <aninhaantunes2000@hotmail.com>
2020-06-24 09:21:01 -0300

    Gestão de rota

--- src/App.js
+++ src/App.js
@@ -2,13 +2,35 @@ import React from 'react';
 import Register from './routers/public/register/views'
 import { Grid } from '@material-ui/core';
 import {makeStyles} from '@material-ui/core/styles';
+import { Route, Redirect, Switch, BrowserRouter } from 'react-router-dom';
+
+function SelectRouter(authentication){
+  if(authentication){
+    return(
+      <Switch>
+        <Route exect path = '/home' component = {<div>Home</div>} />
+      </Switch>  
+    )
+  }
+  return(
+    <Switch>
+      <Route exect path = '/registro' component = {Register} />
+      <Route exect path = '/login' component = {<div>Login</div>}/>
+      <Redirect to = '/login'/>
+    </Switch>  
+  )
+
+}
 
 function App() {
   const classes = useStyles();
+  const routers = SelectRouter(false);
   return (
-    <Grid className = {classes.App}>
-      <Register nome = 'Ana'/>
-    </Grid>
+    <BrowserRouter>
+      <Grid className = {classes.App}>
+        {routers}
+      </Grid>
+    </BrowserRouter>
   );
 }
 
