452db826175237726cc217f98778395b7010a703
DIEGO SANTOS SILVA <diegolv93@eng-aut.grad.ufmg.br>
2020-08-26 19:09:01 -0300

    filtro e validação de string

--- src/shared/utils/validation.js
+++ src/shared/utils/validation.js
@@ -1,4 +1,11 @@
-export const validationEmail = email => {
-    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-  };
+export const validationEmail = (email) => {
+  var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+export const checkStringContainsLetter = (string, text) => {
+  if (string !== null && text !== null) {
+    return string.toString().toUpperCase().includes(text.toUpperCase());
+  }
+  return false;
+};
